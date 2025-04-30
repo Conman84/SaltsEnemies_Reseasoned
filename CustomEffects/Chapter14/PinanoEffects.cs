7f751f6ca76f25ab8f13506b753dc7ece955939a
pymn <154932000+yttpr@users.noreply.github.com>
2025-04-30 14:03:42 -0700

    pinano effects

---
+++ CustomEffects/Chapter14/PinanoEffects.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace SaltEnemies_Reseasoned
+{
+    public class ShowViolentPassiveEffect : EffectSO
+    {
+        public Sprite image;
+        public override bool PerformEffect(CombatStats stats, IUnit caster, TargetSlotInfo[] targets, bool areTargetSlots, int entryVariable, out int exitAmount)
+        {
+            exitAmount = 0;
+            CombatManager.Instance.AddUIAction(new ShowPassiveInformationUIAction(caster.ID, caster.IsUnitCharacter, "Violent (" + entryVariable.ToString() + ")", image));
+            return true;
+        }
+    }
+    public class HasHealthEffectCondition : EffectConditionSO
+    {
+        public override bool MeetCondition(IUnit caster, EffectInfo[] effects, int currentIndex)
+        {
+            return caster.CurrentHealth > 0;
+        }
+    }
+}
