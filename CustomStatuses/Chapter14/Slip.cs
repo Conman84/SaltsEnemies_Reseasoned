91f1798fd19e5846f7abac96c9a22d325e89f9aa
Aprils <154932000+yttpr@users.noreply.github.com>
2025-05-04 00:55:38 -0700

    slip fixed :)

--- CustomStatuses/Chapter14/Slip.cs
+++ CustomStatuses/Chapter14/Slip.cs
@@ -2,6 +2,7 @@
 using MonoMod.RuntimeDetour;
 using SaltsEnemies_Reseasoned;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
@@ -14,6 +15,7 @@ namespace SaltEnemies_Reseasoned
         public static string FieldID => "Slip_ID";
         public static string Intent => "Field_Slip";
         public static SlipFE_SO Object;
+        public static string Trigger => "Slip_Trigger";
         public static void Add()
         {
             SlotStatusEffectInfoSO SlipInfo = ScriptableObject.CreateInstance<SlotStatusEffectInfoSO>();
@@ -60,9 +62,11 @@ namespace SaltEnemies_Reseasoned
         public override bool IsPositive => false;
         public override void OnSlotEffectorTriggerAttached(FieldEffect_Holder holder)
         {
+            CombatManager.Instance.AddObserver(holder.OnEventTriggered_02, Slip.Trigger, holder.Effector);
         }
         public override void OnSlotEffectorTriggerDettached(FieldEffect_Holder holder)
         {
+            CombatManager.Instance.RemoveObserver(holder.OnEventTriggered_02, Slip.Trigger, holder.Effector);
         }
         public override void OnTriggerAttached(FieldEffect_Holder holder, IUnit caller)
         {
@@ -77,15 +81,22 @@ namespace SaltEnemies_Reseasoned
             if (holder.m_ContentMain + holder.Restrictor <= 0) return;
             if (sender is IUnit unit && args is IntegerReference oldslot && oldslot.value != unit.SlotID)
             {
-                ReduceDuration(holder);
-                CombatStats stats = CombatManager.Instance._stats;
 
-                int num = oldslot.value < unit.SlotID ? (unit.IsUnitCharacter ? 1 : unit.Size) : (-1);
+                CombatManager.Instance.AddSubAction(new SlipAction(unit.SlotID, unit, oldslot.value < unit.SlotID, holder.Effector as CombatSlot));
+                return;
+
+                CombatStats stats = CombatManager.Instance._stats;
+                SwapToOneSideEffect perform = ScriptableObject.CreateInstance<SwapToOneSideEffect>();
+                perform._swapRight = oldslot.value < unit.SlotID;
+                perform.PerformEffect(stats, unit, (holder.Effector as CombatSlot).TargetSlotInformation.SelfArray(), true, 1, out int exitAmount);
+
+                /*int num = oldslot.value < unit.SlotID ? (unit.IsUnitCharacter ? 1 : unit.Size) : (-1);
                 if (unit.IsUnitCharacter)
                 {
                     if (unit.SlotID + num >= 0 && unit.SlotID + num < stats.combatSlots.CharacterSlots.Length)
                     {
                         stats.combatSlots.SwapCharacters(unit.SlotID, unit.SlotID + num, isMandatory: true);
+
                     }
                 }
                 else
@@ -94,8 +105,12 @@ namespace SaltEnemies_Reseasoned
                     {
                         stats.combatSlots.SwapEnemies(unit.SlotID, firstSlotSwap, unit.SlotID + num, secondSlotSwap);
                     }
-                }
+                }*/
             }
+        }
+        public override void OnEventCall_02(FieldEffect_Holder holder, object sender, object args)
+        {
+            ReduceDuration(holder);
         }
         public override void ReduceDuration(FieldEffect_Holder holder)
         {
@@ -105,6 +120,31 @@ namespace SaltEnemies_Reseasoned
             {
                 holder.Effector.FieldEffectValuesChanged(_FieldID, useSpecialSound: false, holder.m_ContentMain - contentMain);
             }
+        }
+    }
+    public class SlipAction : CombatAction
+    {
+        public int originSlot;
+        public CombatSlot slot;
+        public IUnit unit;
+        public bool swapRight;
+        public SlipAction(int origin, IUnit unit, bool right, CombatSlot slot)
+        {
+            originSlot = origin;
+            this.unit = unit;
+            swapRight = right;
+            this.slot = slot;
+        }
+        public override IEnumerator Execute(CombatStats stats)
+        {
+            if (unit.SlotID == originSlot && slot.ContainsFieldEffect(Slip.FieldID))
+            {
+                CombatManager.Instance.PostNotification(Slip.Trigger, slot, null);
+                SwapToOneSideEffect perform = ScriptableObject.CreateInstance<SwapToOneSideEffect>();
+                perform._swapRight = swapRight;
+                perform.PerformEffect(stats, unit, slot.TargetSlotInformation.SelfArray(), true, 1, out int exitAmount);
+            }
+            yield return null;
         }
     }
     public class ApplySlipSlotEffect : FieldEffect_Apply_Effect
