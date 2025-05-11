3e26ffd34ebfd982f13b97be8d3c41b8201f7f50
Aprils <154932000+yttpr@users.noreply.github.com>
2025-05-10 23:14:51 -0700

    foxtrot assets

--- Scripts/Template.cs
+++ Scripts/Template.cs
@@ -38,31 +38,8 @@ namespace SaltsEnemies_Reseasoned
 
 /*-------------------EXTRA STUFF: GROUP 19 PATHS (all in meowy)-----------------*/
 
-//Assets/enem3/Phone_Enemy.prefab
-//Assets/gib3/Phone_Gibs.prefab
-
-//Assets/enem3/Starless_Enemy.prefab
-//Assets/gib3/Starless_Gibs.prefab
-
-//Assets/enem3/Eyeless_Enemy.prefab
-//Assets/gib3/EyelessGibs.prefab
-
-//Assets/enem3/Pawn_Enemy.prefab
-//Assets/gib3/Pawn_Gibs.prefab
-
-//event:/Hawthorne/PawnSong
-
-//Assets/enem3/Yang_Enemy.prefab
-//Assets/gib3/Yang_Gibs.prefab
-
-//Assets/enem3/Yin_Enemy.prefab
-//Assets/gib3/Yin_Gibs.prefab
-
-//event:/Hawthorne/BishopSong
-
-//Assets/enem3/2009_Enemy.prefab
-//m_Locator.Find("Sprite").Find("Face").GetComponent
-//Assets/gib3/2009_Gibs.prefab
-
-//Assets/enem3/Chiito_Enemy.prefab
-//Assets/gib3/Chiito_Gibs.prefab
+//Assets/enem3/Foxtrot_Enemy.prefab
+//Sprite => Handle => Outline
+//Assets/gib3/Foxtrot_Gibs.prefab
+//event:/Hawthorne/Sund/FoxtrotHit
+//event:/Hawthorne/Sund/FoxtrotDie
