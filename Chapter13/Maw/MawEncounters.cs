961710dc028dc90c1f7defbb8e70ccb653a4e812
Aprils <154932000+yttpr@users.noreply.github.com>
2025-05-29 13:43:06 -0700

    maw work

---
+++ Chapter13/Maw/MawEncounters.cs
@@ -0,0 +1,22 @@
+﻿using BrutalAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SaltsEnemies_Reseasoned
+{
+    public static class MawEncounters
+    {
+        public static void Add()
+        {
+            Portals.AddPortalSign("Salt_TemplateEncounter_Sign", ResourceLoader.LoadSprite("TemplateWorld.png"), Portals.EnemyIDColor);
+
+            EnemyEncounter_API med = new EnemyEncounter_API(EncounterType.Random, Orph.H.Maw.Med, "Salt_TemplateEncounter_Sign");
+            med.MusicEvent = "event:/Hawthorne/NewCoffinTheme";
+            med.RoarEvent = LoadedAssetsHandler.GetEnemy("Visage_MyOwn_EN").deathSound;
+
+            med.AddEncounterToDataBases();
+            EnemyEncounterUtils.AddEncounterToZoneSelector(Orph.H.Maw.Med, 8, ZoneType_GameIDs.Garden_Hard, BundleDifficulty.Medium);
+        }
+    }
+}
