ed927951d2693a5d748b4204bd9e634352d81ed7
Aprils <154932000+yttpr@users.noreply.github.com>
2025-05-28 21:10:36 -0700

    firebird encounter work

--- Chapter12/Firebird/FirebirdEncounters.cs
+++ Chapter12/Firebird/FirebirdEncounters.cs
@@ -18,6 +18,19 @@ namespace SaltEnemies_Reseasoned
 
             med.AddRandomEncounter("Firebird_EN", "InHisImage_EN", "InHisImage_EN");
             med.AddRandomEncounter("Firebird_EN", "InHerImage_EN", "InHerImage_EN");
+            med.SimpleAddEncounter(1, "Firebird_EN", 3, "EyePalm_EN");
+            med.AddRandomEncounter("Firebird_EN", "Shua_EN", "LittleAngel_EN");
+            med.AddRandomEncounter("Firebird_EN", "MiniReaper_EN", "MiniReaper_EN");
+            med.SimpleAddEncounter(1, "Firebird_EN", 3, Enemies.Camera);
+            med.AddRandomEncounter("Firebird_EN", "Hunter_EN", "Damocles_EN", "Damocles_EN");
+            med.AddRandomEncounter("Firebird_EN", "WindSong_EN", "Hunter_EN");
+            med.AddRandomEncounter("Firebird_EN", "Grandfather_EN", Flower.Blue);
+            med.AddRandomEncounter("Firebird_EN", Flower.Grey, "EyePalm_EN", "EyePalm_EN");
+            med.AddRandomEncounter("Firebird_EN", Flower.Red, Flower.Blue);
+            med.AddRandomEncounter("Firebird_EN", Spoggle.Grey, "ChoirBoy_EN");
+            med.AddRandomEncounter("Firebird_EN", "ChoirBoy_EN", "ChoirBoy_EN");
+            med.AddRandomEncounter("Firebird_EN", "WindSong_EN", "ChoirBoy_EN");
+            med.AddRandomEncounter("Firebird_EN", "ChoirBoy_EN", "GlassFigurine_EN");
 
             med.AddEncounterToDataBases();
             EnemyEncounterUtils.AddEncounterToZoneSelector(Garden.H.Firebird.Med, 6, ZoneType_GameIDs.Garden_Hard, BundleDifficulty.Medium);
