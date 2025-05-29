b4bfb06014182799412e770f4b7b7ff8ea069f62
Aprils <154932000+yttpr@users.noreply.github.com>
2025-05-29 11:48:22 -0700

    indicator encounter work more

--- Chapter13/Indicator/IndicatorEncounters.cs
+++ Chapter13/Indicator/IndicatorEncounters.cs
@@ -21,9 +21,44 @@ namespace SaltsEnemies_Reseasoned
             easy.AddRandomEncounter("Indicator_EN", "BlackStar_EN", "BlackStar_EN");
             easy.AddRandomEncounter("Indicator_EN", "EyePalm_EN", "EyePalm_EN");
             easy.SimpleAddEncounter(1, "Indicator_EN", 4, "NextOfKin_EN");
+            easy.SimpleAddEncounter(1, "Indicator_EN", 2, Enemies.Shivering);
+            easy.SimpleAddEncounter(1, "Indicator_EN", 2, "EyePalm_EN", 1, "Skyloft_EN");
+            easy.SimpleAddEncounter(1, "Indicator_EN", 2, "EyePalm_EN", 1, "Damocles_EN");
+            easy.SimpleAddEncounter(1, "Indicator_EN", 2, "EyePalm_EN", 1, "BlackStar_EN");
+            easy.AddRandomEncounter("Indicator_EN", Enemies.Camera, Enemies.Camera);
+            easy.SimpleAddEncounter(1, "Indicator_EN", 2, Enemies.Shivering, 1, "Damocles_EN");
+            easy.SimpleAddEncounter(1, "Indicator_EN", 2, Enemies.Shivering, 1, "BlackStar_EN");
+            easy.SimpleAddEncounter(1, "Indicator_EN", 2, Enemies.Shivering, 1, "GlassFigurine_EN");
 
             easy.AddEncounterToDataBases();
             EnemyEncounterUtils.AddEncounterToZoneSelector(Garden.H.Indicator.Easy, 5, ZoneType_GameIDs.Garden_Hard, BundleDifficulty.Easy);
+        }
+        public static void Add_Med()
+        {
+            Portals.AddPortalSign("Salt_IndicatorEncounter_Sign", ResourceLoader.LoadSprite("IndicatorWorld.png"), Portals.EnemyIDColor);
+
+            EnemyEncounter_API easy = new EnemyEncounter_API(EncounterType.Random, Garden.H.Indicator.Med, "Salt_IndicatorEncounter_Sign");
+            easy.MusicEvent = "event:/Hawthorne/IndicatorSong";
+            easy.RoarEvent = LoadedAssetsHandler.GetCharacter("Hans_CH").deathSound;
+
+            easy.AddRandomEncounter("Indicator_EN", "Grandfather_EN");
+            easy.AddRandomEncounter("Indicator_EN", "GlassFigurine_EN");
+            easy.AddRandomEncounter("Indicator_EN", "BlackStar_EN", "BlackStar_EN");
+            easy.AddRandomEncounter("Indicator_EN", "EyePalm_EN", "EyePalm_EN");
+            easy.SimpleAddEncounter(1, "Indicator_EN", 4, "NextOfKin_EN");
+            easy.SimpleAddEncounter(1, "Indicator_EN", 2, Enemies.Shivering);
+            easy.SimpleAddEncounter(1, "Indicator_EN", 3, Enemies.Shivering);
+            easy.SimpleAddEncounter(1, "Indicator_EN", 3, "EyePalm_EN");
+            easy.SimpleAddEncounter(1, "Indicator_EN", 2, "EyePalm_EN", 1, "Skyloft_EN");
+            easy.SimpleAddEncounter(1, "Indicator_EN", 2, "EyePalm_EN", 1, "Damocles_EN");
+            easy.SimpleAddEncounter(1, "Indicator_EN", 2, "EyePalm_EN", 1, "BlackStar_EN");
+            easy.AddRandomEncounter("Indicator_EN", Enemies.Camera, Enemies.Camera);
+            easy.SimpleAddEncounter(1, "Indicator_EN", 2, Enemies.Shivering, 1, "Damocles_EN");
+            easy.SimpleAddEncounter(1, "Indicator_EN", 2, Enemies.Shivering, 1, "BlackStar_EN");
+            easy.SimpleAddEncounter(1, "Indicator_EN", 2, Enemies.Shivering, 1, "GlassFigurine_EN");
+
+            easy.AddEncounterToDataBases();
+            EnemyEncounterUtils.AddEncounterToZoneSelector(Garden.H.Indicator.Med, 5, ZoneType_GameIDs.Garden_Hard, BundleDifficulty.Medium);
         }
     }
 }
