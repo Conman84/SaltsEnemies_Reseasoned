28ceaa445dd3dcace694e27f0b299c2b284605e8
Aprils <154932000+yttpr@users.noreply.github.com>
2025-05-30 02:27:46 -0700

    ynl encounter prework

---
+++ Chapter14/YNL/YNLEncounters.cs
@@ -0,0 +1,22 @@
+﻿using BrutalAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SaltsEnemies_Reseasoned
+{
+    public static class YourNewLifeEncounters
+    {
+        public static void Add()
+        {
+            Portals.AddPortalSign("Salt_YourNewLifeEncounter_Sign", ResourceLoader.LoadSprite("LobotomyPortal.png"), Portals.EnemyIDColor);
+
+            EnemyEncounter_API med = new EnemyEncounter_API(EncounterType.Random, Garden.H.YNL.Med, "Salt_YourNewLifeEncounter_Sign");
+            med.MusicEvent = "event:/Hawthorne/LobotomyTheme";
+            med.RoarEvent = LoadedAssetsHandler.GetEnemy("ChoirBoy_EN").deathSound;
+
+            med.AddEncounterToDataBases();
+            EnemyEncounterUtils.AddEncounterToZoneSelector(Garden.H.YNL.Med, 10, ZoneType_GameIDs.Garden_Hard, BundleDifficulty.Medium);
+        }
+    }
+}
