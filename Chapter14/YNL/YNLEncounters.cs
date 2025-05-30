8540750d537871ab7958b6ecc8a601d2b36fd67d
Aprils <154932000+yttpr@users.noreply.github.com>
2025-05-30 02:39:02 -0700

    ynl encouner work

--- Chapter14/YNL/YNLEncounters.cs
+++ Chapter14/YNL/YNLEncounters.cs
@@ -1,4 +1,5 @@
 ﻿using BrutalAPI;
+using SaltEnemies_Reseasoned;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -14,6 +15,17 @@ namespace SaltsEnemies_Reseasoned
             EnemyEncounter_API med = new EnemyEncounter_API(EncounterType.Random, Garden.H.YNL.Med, "Salt_YourNewLifeEncounter_Sign");
             med.MusicEvent = "event:/Hawthorne/LobotomyTheme";
             med.RoarEvent = LoadedAssetsHandler.GetEnemy("ChoirBoy_EN").deathSound;
+
+            med.AddRandomEncounter("YNL_EN", "InHerImage_EN", "InHerImage_EN");
+            med.AddRandomEncounter("YNL_EN", "InHisImage_EN", "InHisImage_EN", "InHisImage-EN");
+            med.SimpleAddEncounter(1, "YNL_EN", 4, "NextOfKin_EN");
+            med.AddRandomEncounter("YNL_EN", "ChoirBoy_EN", "InHisImage_EN", "InHisImage_EN");
+            med.AddRandomEncounter("YNL_EN", "InHerImage_EN", "InHerImage_EN", "LittleAngel_EN");
+            med.AddRandomEncounter("YNL_EN", "InHisImage_EN", "InHisImage_EN", Jumble.Grey);
+            med.AddRandomEncounter("YNL_EN", Flower.Red, Flower.Blue, "LittleAngel_EN");
+            med.AddRandomEncounter("YNL_EN", "Grandfather_EN", Flower.Red);
+            med.AddRandomEncounter("YNL_EN", "Grandfather_EN", Flower.Blue);
+            med.AddRandomEncounter("YNL_EN", "WindSong_EN", "InHisImage_EN", "InHisImage_EN");
 
             med.AddEncounterToDataBases();
             EnemyEncounterUtils.AddEncounterToZoneSelector(Garden.H.YNL.Med, 10, ZoneType_GameIDs.Garden_Hard, BundleDifficulty.Medium);
