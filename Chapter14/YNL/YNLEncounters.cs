9322a2f766a245ca9ee44524e3d136551952e0c0
Aprils <154932000+yttpr@users.noreply.github.com>
2025-05-30 11:10:09 -0700

    ynl encounter work

--- Chapter14/YNL/YNLEncounters.cs
+++ Chapter14/YNL/YNLEncounters.cs
@@ -2,7 +2,6 @@
 using SaltEnemies_Reseasoned;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace SaltsEnemies_Reseasoned
 {
@@ -26,6 +25,30 @@ namespace SaltsEnemies_Reseasoned
             med.AddRandomEncounter("YNL_EN", "Grandfather_EN", Flower.Red);
             med.AddRandomEncounter("YNL_EN", "Grandfather_EN", Flower.Blue);
             med.AddRandomEncounter("YNL_EN", "WindSong_EN", "InHisImage_EN", "InHisImage_EN");
+            med.AddRandomEncounter("YNL_EN", Flower.Grey, "InHisImage_EN", "InHisImage_EN");
+            med.AddRandomEncounter("YNL_EN", "MiniReaper_EN", Enemies.Shivering, Enemies.Shivering);
+            med.AddRandomEncounter("YNL_EN", "Shua_EN", "WindSong_EN");
+            med.AddRandomEncounter("YNL_EN", "Shua_EN", "Damocles_EN", "Damocles_EN");
+            med.AddRandomEncounter("YNL_EN", Flower.Red, "Damocles_EN", "Damocles_EN");
+            med.AddRandomEncounter("YNL_EN", "InHisImage_EN", "InHisImage_EN", "Damocles_EN");
+            med.AddRandomEncounter("YNL_EN", Flower.Blue, "GlassFigurine_EN");
+            med.AddRandomEncounter("YNL_EN", "InHisImage_EN", "InHisImage_EN", "BlackStar_EN");
+            med.AddRandomEncounter("YNL_EN", "Shua_EN", "BlackStar_EN", "BlackStar_EN");
+            med.AddRandomEncounter("YNL_EN", "Hunter_EN", "InHerImage_EN", "InHerImage_EN");
+            med.AddRandomEncounter("YNL_EN", "Firebird_EN", "ChoirBoy_EN");
+            med.AddRandomEncounter("YNL_EN", "Shua_EN", "ChoirBoy_EN");
+            med.AddRandomEncounter("YNL_EN", "Indicator_EN", "InHisImage_EN", "InHisImage_EN");
+            med.AddRandomEncounter("YNL_EN", "ChoirBoy_EN", "Grandfather_EN");
+            med.AddRandomEncounter("YNL_EN", "InHisImage_EN", "InHisImage_EN", Enemies.Minister);
+            med.AddRandomEncounter("YNL_EN", "Grandfather_EN", Enemies.Minister);
+            med.AddRandomEncounter("YNL_EN", Enemies.Minister, "Damocles_EN", "Damocles_EN");
+            med.AddRandomEncounter("YNL_EN", Enemies.Minister, "WindSong_EN");
+            med.AddRandomEncounter("YNL_EN", Enemies.Minister, "LittleAngel_EN");
+            med.AddRandomEncounter("YNL_EN", Enemies.Minister, "ChoirBoy_EN");
+            med.SimpleAddEncounter(1, "YNL_EN", 3, "EyePalm_EN");
+            med.AddRandomEncounter("YNL_EN", "EyePalm_EN", "InHisImage_EN", "InHisImage_EN");
+            med.AddRandomEncounter("YNL_EN", "EyePalm_EN", "EyePalm_EN", "MiniReaper_EN");
+            med.AddRandomEncounter("YNL_EN", "EyePalm_EN", "EyePalm_EN", "Hunter_EN");
 
             med.AddEncounterToDataBases();
             EnemyEncounterUtils.AddEncounterToZoneSelector(Garden.H.YNL.Med, 10, ZoneType_GameIDs.Garden_Hard, BundleDifficulty.Medium);
