8ce4b4b0327d18773a0cd479971f26eafa3e5e08
Aprils <154932000+yttpr@users.noreply.github.com>
2025-05-31 00:00:11 -0700

    crystal winter detector

---
+++ CustomEffects/Chapter16/CrystalEffects2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SaltEnemies_Reseasoned
+{
+    public static class Winter
+    {
+        public static int Mod
+        {
+            get
+            {
+                switch (DateTime.Now.Month)
+                {
+                    case 12: return 4;
+                    case 11: return 4;
+                    case 10: return 4;
+                    case 1: return 4;
+                    case 2: return 4;
+                    case 3: return 4;
+                    default: return 1;
+                }
+            }
+        }
+    }
+}
