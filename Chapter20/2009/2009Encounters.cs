f4ef8e3a83c0e5c4f3ddbb49c953d138bf5982c1
Aprils <154932000+yttpr@users.noreply.github.com>
2025-06-01 13:44:28 -0700

    2009 encounter prework

---
+++ Chapter20/2009/2009Encounters.cs
@@ -0,0 +1,22 @@
+﻿using BrutalAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SaltsEnemies_Reseasoned
+{
+    public static class TwoThousandNineEncounters
+    {
+        public static void Add()
+        {
+            Portals.AddPortalSign("Salt_2009Encounter_Sign", ResourceLoader.LoadSprite("2009World.png"), Portals.EnemyIDColor);
+
+            EnemyEncounter_API med = new EnemyEncounter_API(EncounterType.Random, Shore.H.TwoThousandNine.Med, "Salt_2009Encounter_Sign");
+            med.MusicEvent = "event:/Hawthorne/2009Theme";
+            med.RoarEvent = "event:/Hawthorne/Roar/PixelRoar";
+
+            med.AddEncounterToDataBases();
+            EnemyEncounterUtils.AddEncounterToZoneSelector(Shore.H.TwoThousandNine.Med, 25, ZoneType_GameIDs.FarShore_Hard, BundleDifficulty.Medium);
+        }
+    }
+}
