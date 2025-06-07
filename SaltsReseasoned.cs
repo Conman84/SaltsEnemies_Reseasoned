6dbdd7925aae4d29d89043a96efd8fbd2d7dfb29
Aprils <154932000+yttpr@users.noreply.github.com>
2025-06-06 20:49:31 -0700

    updated version #

--- SaltsReseasoned.cs
+++ SaltsReseasoned.cs
@@ -21,7 +21,7 @@ using static UnityEngine.EventSystems.EventTrigger;
 namespace SaltsEnemies_Reseasoned
 {
 
-    [BepInPlugin("000.saltenemies", "Salt Enemies (TM) Reseasoned", "1.0.0")]
+    [BepInPlugin("000.saltenemies", "Salt Enemies (TM) Reseasoned", "1.0.1")]
     [BepInDependency("BrutalOrchestra.BrutalAPI", BepInDependency.DependencyFlags.HardDependency)]
     public class SaltsReseasoned : BaseUnityPlugin
     {
