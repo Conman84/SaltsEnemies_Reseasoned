1f83166acdd0eca246af36c5c06f5acf59151bb2
Aprils <154932000+yttpr@users.noreply.github.com>
2025-06-10 03:54:27 -0700

    eggkeeper crossover prework

---
+++ Crossovers/EggKeeper/EggKeeper_9_10_Crossoveer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SaltEnemies_Reseasoned
+{
+    public static class EggKeeper_9_10_Crossover
+    {
+        public static void Add()
+        {
+
+        }
+    }
+}
