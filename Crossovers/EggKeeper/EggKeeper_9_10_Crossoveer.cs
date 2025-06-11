952eba1dee09e60e1679c9d436b5d0bb058a982b
Aprils <154932000+yttpr@users.noreply.github.com>
2025-06-11 11:04:00 -0700

    more eggkeeper work

--- Crossovers/EggKeeper/EggKeeper_9_10_Crossoveer.cs
+++ Crossovers/EggKeeper/EggKeeper_9_10_Crossoveer.cs
@@ -15,6 +15,18 @@ namespace SaltEnemies_Reseasoned
             med.AddRandomGroup("Grandfather_EN", Flower.Red, "EggKeeper_EN", Enemies.Shivering);
             med.AddRandomGroup("Grandfather_EN", "Shua_EN", "EggKeeper_EN", Enemies.Shivering);
             med.AddRandomGroup("Grandfather_EN", "ChoirBoy_EN", "EggKeeper_EN", Enemies.Shivering);
+
+            med = new AddTo(Garden.H.Flower.Grey.Med);
+            med.AddRandomGroup(Flower.Grey, "InHisImage_EN", "InHerImage_EN", "EggKeeper_EN");
+
+            med = new AddTo(Garden.H.MiniReaper.Med);
+            med.AddRandomGroup("MiniReaper_EN", "InHerImage_EN", "InHerImage_EN", "EggKeeper_EN");
+            med.AddRandomGroup("MiniReaper_EN", "InHerImage_EN", "InHisImage_EN", "EggKeeper_EN");
+            med.AddRandomGroup("MiniReaper_EN", Enemies.Shivering, "EyePalm_EN", "EggKeeper_EN");
+            med.AddRandomGroup("MiniReaper_EN", "Grandfather_EN", "EggKeeper_EN", Flower.Blue);
+
+            AddTo easy = new AddTo(Garden.H.EyePalm.Easy);
+            easy.AddRandomGroup("EyePalm_EN", "EyePalm_EN", "EggKeeper_EN", "EyePalm_EN");
         }
     }
 }
