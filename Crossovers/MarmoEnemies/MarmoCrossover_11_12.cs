ff2097ef03f46874d5904e7f2bbbc2f02f83c8fe
Aprils <154932000+yttpr@users.noreply.github.com>
2025-06-12 16:17:31 -0700

    marmo crossover encunter work

--- Crossovers/MarmoEnemies/MarmoCrossover_11_12.cs
+++ Crossovers/MarmoEnemies/MarmoCrossover_11_12.cs
@@ -19,6 +19,8 @@ namespace SaltsEnemies_Reseasoned
             hard.AddRandomGroup("Tripod_EN", "Skyloft_EN", "Snaurce_EN", "Snaurce_EN");
             hard.AddRandomGroup("Tripod_EN", Enemies.Mungling, "Snaurce_EN");
             hard.AddRandomGroup("Tripod_EN", "AFlower_EN", "Surimi_EN");
+            hard.AddRandomGroup("Tripod_EN", Jumble.Red, Jumble.Yellow, Jumble.Unstable);
+            hard.AddRandomGroup("Tripod_EN", "LittleBeak_EN", Spoggle.Unstable);
 
             hard = new AddTo(Shore.H.Warbird.Hard);
             hard.AddRandomGroup("Warbird_EN", "LittleBeak_EN", "Snaurce_EN");
@@ -27,6 +29,10 @@ namespace SaltsEnemies_Reseasoned
             hard.AddRandomGroup("Warbird_EN", "Surimi_EN", "FlaMinGoa_EN");
             hard.AddRandomGroup("Warbird_EN", Spoggle.Yellow, Spoggle.Blue, "Snaurce_EN");
             hard.AddRandomGroup("Warbird_EN", Jumble.Red, "Surimi_EN", "Surimi_EN");
+            hard.AddRandomGroup("Warbird_EN", Jumble.Red, Jumble.Yellow, Jumble.Unstable);
+            hard.AddRandomGroup("Warbird_EN", Spoggle.Yellow, Spoggle.Unstable, "LostSheep_EN");
+            hard.AddRandomGroup("Warbird_EN", Spoggle.Blue, Spoggle.Unstable, "LostSheep_EN");
+            hard.AddRandomGroup("Warbird_EN", "LittleBeak_EN", Jumble.Unstable);
 
             AddTo med = new AddTo(Shore.H.LittleBeak.Med);
             med.AddRandomGroup("LittleBeak_EN", "Snaurce_EN", "Snaurce_EN");
@@ -35,6 +41,9 @@ namespace SaltsEnemies_Reseasoned
             med.AddRandomGroup("LittleBeak_EN", "Snaurce_EN", "Skyloft_EN", "Mung_EN");
             med.AddRandomGroup("LittleBeak_EN", "Surimi_EN", Enemies.Mungling);
             med.AddRandomGroup("LittleBeak_EN", "Surimi_EN", "Surimi_EN");
+            med.AddRandomGroup("LittleBeak_EN", Jumble.Unstable, Jumble.Yellow);
+            if (SaltsReseasoned.silly > 50) med.AddRandomGroup("LittleBeak_EN", Spoggle.Unstable, Spoggle.Blue);
+            if (SaltsReseasoned.silly < 50) med.AddRandomGroup("LittleBeak_EN", Spoggle.Unstable, Spoggle.Yellow);
 
             hard = new AddTo(Shore.H.Angler.Hard);
             if (SaltsReseasoned.trolling > 50) hard.AddRandomGroup("AFlower_EN", "LittleBeak_EN", "Snaurce_EN");
