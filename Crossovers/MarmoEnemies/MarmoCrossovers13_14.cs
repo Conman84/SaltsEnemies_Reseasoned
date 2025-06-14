8e3f6a138193e95a48120e2a0955d72edcdfb340
Aprils <154932000+yttpr@users.noreply.github.com>
2025-06-13 19:20:20 -0700

    marmo 13 14 crossover prework

---
+++ Crossovers/MarmoEnemies/MarmoCrossovers13_14.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SaltsEnemies_Reseasoned
+{
+    public static class MarmoCrossovers13_14
+    {
+        public static void Add()
+        {
+
+        }
+        public static void FarShore()
+        {
+
+        }
+    }
+}
