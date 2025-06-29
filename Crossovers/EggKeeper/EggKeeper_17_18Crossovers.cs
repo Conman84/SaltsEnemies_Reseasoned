0b219f4aad3636aff6b8deeb00af65f5223ee31c
Aprils <154932000+yttpr@users.noreply.github.com>
2025-06-28 18:03:34 -0700

    removed awake wall from sepulchre poo

---
+++ Crossovers/EggKeeper/EggKeeper_17_18Crossovers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SaltsEnemies_Reseasoned.Crossovers
+{
+    public static class EggKeeper_17_18Crossovers
+    {
+        public static void Add()
+        {
+
+        }
+    }
+}
