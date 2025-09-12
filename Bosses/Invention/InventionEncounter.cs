ee8f5c621b0c98e6c6269ba60810b2e8eef300a0
Aprils <154932000+yttpr@users.noreply.github.com>
2025-09-12 11:24:04 -0700

    bossid

--- Bosses/Invention/InventionEncounter.cs
+++ Bosses/Invention/InventionEncounter.cs
@@ -15,6 +15,7 @@ namespace SaltsEnemies_Reseasoned
             EnemyEncounter_API boss = new EnemyEncounter_API(EncounterType.Random, "BOSS_Zone02_Invention_EnemyBundle", "Salt_InventionEncounter_Sign");
             boss.MusicEvent = "event:/Blackwater/InventionSong";
             boss.RoarEvent = LoadedAssetsHandler.GetEnemy("Visage_MyOwn_EN").deathSound;
+            boss.BossID = "Invention_BOSS";
 
             boss.AddRandomEncounter("Invention_BOSS");
 
