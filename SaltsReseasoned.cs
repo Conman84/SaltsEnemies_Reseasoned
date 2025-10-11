6bd8b36a2d6ecc9a6b5812dd5bc46b414b7d7a21
Aprils <154932000+yttpr@users.noreply.github.com>
2025-10-10 17:22:42 -0700

    wording for baddog passive

--- SaltsReseasoned.cs
+++ SaltsReseasoned.cs
@@ -516,7 +516,7 @@ namespace SaltsEnemies_Reseasoned
             AddPassivesToGlossary.AddPassive(ResourceLoader.LoadSprite("BlackstarPassive.png"), "Turbulent", "On being directly damaged, shuffle all enemy positions.");
             AddPassivesToGlossary.AddPassive(ResourceLoader.LoadSprite("IndicatorPassive.png"), "Compulsory", "On being directly damaged, force the Opposing unit to perform a random ability.");
             AddPassivesToGlossary.AddPassive(ResourceLoader.LoadSprite("SpasmPassive.png"), "Spasm", "On death, give all allies 1-2 additional actions on the timeline.");
-            AddPassivesToGlossary.AddPassive(ResourceLoader.LoadSprite("MawPassive.png"), "Bad Dog", "On anything moving during the player's turn, if this unit has an Opposing target, remove all of its actions from the timeline. \nOtherwise, return all lost actions.");
+            AddPassivesToGlossary.AddPassive(ResourceLoader.LoadSprite("MawPassive.png"), "Bad Dog", "At the start of each round and on anything moving during the player's turn, if this enemy has an Opposing party member, remove all of its actions from the timeline. \nOtherwise, return all lost actions.");
             AddPassivesToGlossary.AddPassive(ResourceLoader.LoadSprite("SkinPeelingPassive.png"), "Skinning", "On being directly damaged, exhaust the Opposing party member's ability usage.");
 
             //CH14
