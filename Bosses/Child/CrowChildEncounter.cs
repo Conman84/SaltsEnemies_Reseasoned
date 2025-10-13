2ca1855a46383675ab02484d1bfdedb5c8f6223a
Aprils <154932000+yttpr@users.noreply.github.com>
2025-10-13 15:12:02 -0700

    crow child done

--- Bosses/Child/CrowChildEncounter.cs
+++ Bosses/Child/CrowChildEncounter.cs
@@ -3,6 +3,7 @@ using SaltEnemies_Reseasoned;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using UnityEngine;
 
 namespace SaltsEnemies_Reseasoned
 {
@@ -18,13 +19,24 @@ namespace SaltsEnemies_Reseasoned
 
             EnemyEncounter_API boss = new EnemyEncounter_API(EncounterType.Specific, "BOSS_Zone01_CrowChild_EnemyBundle", "Salt_CrowChildEncounter_Sign");
             boss.MusicEvent = "event:/Blackwater/CrowChildSong";
-            boss.RoarEvent = LoadedAssetsHandler.GetEnemy("Visage_MyOwn_EN").deathSound;
+            boss.RoarEvent = "event:/Blackwater/Roar/CCRoar";
             boss.BossID = "CrowChild_BOSS";
             boss.AddSpecialEnvironment("CrowChild_Arena");
 
             boss.CreateNewEnemyEncounterData(["CrowChild_BOSS"], [2]);
 
             boss.AddEncounterToDataBases();
+
+            VsBossData vsBossData = new VsBossData();
+            vsBossData.animation = SaltsReseasoned.Dreams.LoadAsset<AnimationClip>("Assets/Bosses/Crow/CrowChildSplash.anim");
+            vsBossData.roarTime = 4f;
+            vsBossData.arenaSprite = ResourceLoader.LoadSprite("CrowChildEnv.png");
+            vsBossData.extraArenaSprite = ResourceLoader.LoadSprite("CrowChildEnv.png");
+            vsBossData.bossSprite = ResourceLoader.LoadSprite("art_crow_head.png");
+            vsBossData.signatureSprite = ResourceLoader.LoadSprite("splash_crow.png");
+            vsBossData.extraSignatureSprite = ResourceLoader.LoadSprite("splash_crow.png");
+            Misc.AddCustom_VSAnimationData("CrowChild_BOSS", vsBossData);
+
             EnemyEncounterUtils.AddEncounterToZoneSelector("BOSS_Zone01_CrowChild_EnemyBundle", 10, ZoneType_GameIDs.FarShore_Hard, BundleDifficulty.Boss);
         }
     }
