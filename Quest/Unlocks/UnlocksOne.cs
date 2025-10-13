8fd5d95dda00df2f9f27d564ab5a81e5343b3967
Aprils <154932000+yttpr@users.noreply.github.com>
2025-10-13 15:31:34 -0700

    the cog

--- Quest/Unlocks/UnlocksOne.cs
+++ Quest/Unlocks/UnlocksOne.cs
@@ -1,4 +1,6 @@
-﻿using SaltEnemies_Reseasoned;
+﻿using BrutalAPI;
+using BrutalAPI.Items;
+using SaltEnemies_Reseasoned;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -16,6 +18,28 @@ namespace SaltsEnemies_Reseasoned
                 "Jumpy_PA", "Lightweight_PA", "Scramble_PA", "Evasive_PA", "Turbulent_PA", "CCTV_PA", "Jittery_PA", "Fluttery_PA", WarpingHandler.Type,
                 "Lonely_PA", "Melancholy_PA", "Gluttony_PA", "Rotary_PA", MarchingHandler.Passive, "Hiding_PA", "Seeking_PA"
                 ];
+
+            PerformEffect_Item cog = new PerformEffect_Item("Aprils_Cog_SW", [Effects.GenerateEffect(cogEffect)]);
+            cog.Name = "The Cog";
+            cog.Flavour = "\"it's a metaphor.\"";
+            cog.Description = "Disable most movement passives.";
+            cog.Icon = ResourceLoader.LoadSprite("item_cog.png");
+            cog.EquippedModifiers = [];
+            cog.TriggerOn = TriggerCalls.OnBeforeCombatStart;
+            cog.DoesPopUpInfo = false;
+            cog.Conditions = [];
+            cog.DoesActionOnTriggerAttached = false;
+            cog.ConsumeOnTrigger = TriggerCalls.Count;
+            cog.ConsumeOnUse = false;
+            cog.ConsumeConditions = [];
+            cog.ShopPrice = 4;
+            cog.IsShopItem = true;
+            cog.StartsLocked = true;
+            cog.OnUnlockUsesTHE = false;
+            cog.UsesSpecialUnlockText = false;
+            cog.SpecialUnlockID = UILocID.None;
+            cog.item._ItemTypeIDs = [];
+            cog.item.AddBlueSkyUnlock("Gospel_CH", "locked_cog.png", "ach_cog.png");
         }
     }
 }
