f129bfa7123ba8c000ebd8868c48724b19f56e92
Aprils <154932000+yttpr@users.noreply.github.com>
2025-10-15 13:05:47 -0700

    updated black and blue arena

--- Bosses/Black/BlackAndBlueEncounters.cs
+++ Bosses/Black/BlackAndBlueEncounters.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using UnityEngine;
 
 namespace SaltsEnemies_Reseasoned
 {
@@ -12,6 +13,7 @@ namespace SaltsEnemies_Reseasoned
             Portals.AddPortalSign("Salt_BlackAndBlueEncounter_Sign", ResourceLoader.LoadSprite("WarCriminalWorld.png"), Portals.BossIDColor);
 
             EnvironmentTools.PrepareCombatEnvPrefab("Assets/Defacer/BB_Arena.prefab", "BlackAndBlue_Arena", SaltsReseasoned.Dreams);
+            LoadedAssetsHandler.TryGetCombatEnvironmentPrefab("BlackAndBlue_Arena").gameObject.SetMinesMaterial();
 
             EnemyEncounter_API boss = new EnemyEncounter_API(EncounterType.Specific, "BOSS_Zone01_BlackAndBlue_EnemyBundle", "Salt_BlackAndBlueEncounter_Sign");
             boss.MusicEvent = "event:/Blackwater/BlackAndBlueSong";
@@ -23,6 +25,17 @@ namespace SaltsEnemies_Reseasoned
 
             boss.AddEncounterToDataBases();
             EnemyEncounterUtils.AddEncounterToZoneSelector("BOSS_Zone01_BlackAndBlue_EnemyBundle", 5, ZoneType_GameIDs.FarShore_Hard, BundleDifficulty.Boss);
+        }
+        public static void SetMinesMaterial(this GameObject self)
+        {
+            Transform holder = self.transform.Find("Mines");
+
+            Material use = BlueSkyEncounters.GettingTheShader();
+
+            for (int i = 0; i < 8; i++)
+            {
+                holder.GetChild(i).GetComponent<SpriteRenderer>().material = use;
+            }
         }
     }
 }
