dcd18fa2eb392012ddbdd5c97ff76a56d9002807
Aprils <154932000+yttpr@users.noreply.github.com>
2025-10-19 13:35:56 -0700

    glass and nameless tweaks

--- Chapter11/Nameless/Nameless.cs
+++ Chapter11/Nameless/Nameless.cs
@@ -25,28 +25,21 @@ namespace SaltsEnemies_Reseasoned
             };
             template.PrepareEnemyPrefab("assets/group4/Nameless/Nameless_Enemy.prefab", SaltsReseasoned.Group4, SaltsReseasoned.Group4.LoadAsset<GameObject>("assets/group4/Nameless/Nameless_Gibs.prefab").GetComponent<ParticleSystem>());
 
-            
+            GenerateRandomManaBetweenEffect randomize = ScriptableObject.CreateInstance<GenerateRandomManaBetweenEffect>();
+            randomize.possibleMana = [Pigments.Red, Pigments.Blue, Pigments.Blue, Pigments.Yellow, Pigments.Yellow, Pigments.Purple, Pigments.Purple];
 
-            //FLITHERING
-            PerformEffectPassiveAbility flither = ScriptableObject.CreateInstance<PerformEffectPassiveAbility>();
-            flither._passiveName = "Flithering";
-            flither.passiveIcon = ResourceLoader.LoadSprite("FlitheringIcon.png");
-            flither.m_PassiveID = FlitheringHandler.Flithering;
-            flither._enemyDescription = "On any enemy dying, if there are no other enemies without \"Withering\" or \"Flithering\" as passives, instantly flee.\n" +
-                "At the start and end of the enemies' turn, if there are no other enemies without \"Cowardice\" or \"Flithering\" as passives, instantly flee.";
-            flither._characterDescription = "doesnt work";
-            flither.doesPassiveTriggerInformationPanel = false;
-            flither.effects = new EffectInfo[] { Effects.GenerateEffect(RootActionEffect.Create(new EffectInfo[]
-            {
-                Effects.GenerateEffect(ScriptableObject.CreateInstance<CowardEffect>(), 1, Slots.Self)
-            }), 1, Slots.Self) };
-            flither._triggerOn = new TriggerCalls[] { TriggerCalls.OnPlayerTurnEnd_ForEnemy, TriggerCalls.OnRoundFinished };
-            flither.conditions = new EffectorConditionSO[]
-            {
-                ScriptableObject.CreateInstance<CowardCondition>()
-            };
+            PerformEffectPassiveAbility fractal = ScriptableObject.CreateInstance<PerformEffectPassiveAbility>();
+            fractal.name = "Fractal_3_PA";
+            fractal._passiveName = "Fractal (3)";
+            fractal.passiveIcon = ResourceLoader.LoadSprite("FractalPassive.png");
+            fractal.m_PassiveID = "Fractal_PA";
+            fractal._enemyDescription = "At the end of the timeline, generate 3 random pigment.";
+            fractal._characterDescription = fractal._enemyDescription;
+            fractal.conditions = [];
+            fractal._triggerOn = [TriggerCalls.TimelineEndReached];
+            fractal.effects = [Effects.GenerateEffect(randomize, 3, Slots.Self)];
 
-            template.AddPassives(new BasePassiveAbilitySO[] { Passives.Fleeting4, Passives.Withering });
+            template.AddPassives(new BasePassiveAbilitySO[] { fractal, Passives.Fleeting4, Passives.Withering });
             //template.CombatExitEffects = Effects.GenerateEffect(ScriptableObject.CreateInstance<SpawnCasterGibsEffect>(), 1, Slots.Self, ScriptableObject.CreateInstance<IsDieCondition>()).SelfArray();
 
 
@@ -88,11 +81,21 @@ namespace SaltsEnemies_Reseasoned
             nobody.AddIntentsToTarget(Targetting.Everything(false), [IntentType_GameIDs.Misc_Hidden.ToString()]);
             nobody.AddIntentsToTarget(Targetting_By_Moved.Create(false), [IntentType_GameIDs.Status_Ruptured.ToString()]);
 
+            Ability cubism = new Ability("Cubibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibibism", "Cubism_A");
+            cubism.Description = "Inflict 10 Entropy on the Opposing party member and this enemy.";
+            cubism.Rarity = Rarity.CreateAndAddCustomRarityToPool("nameless_low", 2);
+            cubism.Effects = [Effects.GenerateEffect(ScriptableObject.CreateInstance<ApplyEntropyEffect>(), 10, MultiTargetting.Create(Slots.Front, Slots.Self))];
+            cubism.AddIntentsToTarget(Slots.Front, [Entropy.Intent]);
+            cubism.AddIntentsToTarget(Slots.Self, [Entropy.Intent]);
+            cubism.Visuals = CustomVisuals.GetVisuals("Salt/Class");
+            cubism.AnimationTarget = Slots.Front;
+
             //ADD ENEMY
             template.AddEnemyAbilities(new EnemyAbilityInfo[]
             {
                 nameless.GenerateEnemyAbility(true),
-                nobody.GenerateEnemyAbility(true)
+                nobody.GenerateEnemyAbility(true),
+                cubism.GenerateEnemyAbility(true),
             });
             template.AddEnemy(true);
         }
