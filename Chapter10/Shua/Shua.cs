32472dad749fdea3489790ff7bfcd7dbe3942d0c
Aprils <154932000+yttpr@users.noreply.github.com>
2025-10-23 23:40:11 -0700

    new shua sounds. need to do: ynl sounds, indicator sounds

--- Chapter10/Shua/Shua.cs
+++ Chapter10/Shua/Shua.cs
@@ -18,8 +18,8 @@ namespace SaltsEnemies_Reseasoned
                 CombatSprite = ResourceLoader.LoadSprite("ShuaIcon.png"),
                 OverworldAliveSprite = ResourceLoader.LoadSprite("ShuaWorld.png", new Vector2(0.5f, 0f), 32),
                 OverworldDeadSprite = ResourceLoader.LoadSprite("ShuaDead.png", new Vector2(0.5f, 0f), 32),
-                DamageSound = LoadedAssetsHandler.GetCharacter("Hans_CH").damageSound,
-                DeathSound = LoadedAssetsHandler.GetCharacter("Hans_CH").deathSound,
+                DamageSound = "event:/Hawthorne/Soisenay/CoraHit",
+                DeathSound = "event:/Hawthorne/Soisenay/CoraDie",
                 AbilitySelector = ScriptableObject.CreateInstance<AbilitySelector_Shua>()
             };
             shua.PrepareEnemyPrefab("assets/group4/Shua/Shua_Enemy.prefab", SaltsReseasoned.Group4, SaltsReseasoned.Group4.LoadAsset<GameObject>("assets/group4/Shua/Shua_Gibs.prefab").GetComponent<ParticleSystem>());
