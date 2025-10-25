82e4dce8a919af22148532f2f264f1d5dc744cff
Aprils <154932000+yttpr@users.noreply.github.com>
2025-10-24 20:21:40 -0700

    heheheha

--- SaltsReseasoned.cs
+++ SaltsReseasoned.cs
@@ -435,6 +435,7 @@ namespace SaltsEnemies_Reseasoned
             {
                 PCall(HauntlingEncounters.Add);
                 PCall(InsiderEncounters.Add);
+                PCall(CorpseChanencounters.Add);
             }
 
             //BOSSES
