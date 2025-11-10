da78ec0b7d64caea9f06391628cc4c2b8e645794
Aprils <154932000+yttpr@users.noreply.github.com>
2025-11-10 11:56:03 -0800

    test2

--- Scripts/VersionCompatabilityExtensions.cs
+++ Scripts/VersionCompatabilityExtensions.cs
@@ -23,9 +23,9 @@ namespace SaltsEnemies_Reseasoned
 
         public static IntegerReference GenerateDamageIntReference(int amount, string damageTypeID, bool directDamage, bool ignoreShield, int affectedStartSlot, int affectedEndSlot, IUnit possibleSourceUnit, IUnit damagedUnit)
         {
-            if (Type.GetType("IntegerReference_Damage", false) != null)
+            if (typeof(IntegerReference).Assembly.GetType("IntegerReference_Damage", false) != null)
             {
-                object ret = Type.GetType("IntegerReference_Damage").GetConstructor([typeof(int), typeof(string), typeof(bool), typeof(bool), typeof(int), typeof(int), typeof(IUnit), typeof(IUnit)]).Invoke([amount, damageTypeID, directDamage, ignoreShield, affectedStartSlot, affectedEndSlot, possibleSourceUnit, damagedUnit]);
+                object ret = typeof(IntegerReference).Assembly.GetType("IntegerReference_Damage").GetConstructor([typeof(int), typeof(string), typeof(bool), typeof(bool), typeof(int), typeof(int), typeof(IUnit), typeof(IUnit)]).Invoke([amount, damageTypeID, directDamage, ignoreShield, affectedStartSlot, affectedEndSlot, possibleSourceUnit, damagedUnit]);
 
                 Debug.Log("exists integerreference_damage");
 
