233e8c095709e44d7580ec44c6a268bbca122614
Aprils <154932000+yttpr@users.noreply.github.com>
2025-11-12 22:39:01 -0800

    test

--- Chapter22/Insider/Insider.cs
+++ Chapter22/Insider/Insider.cs
@@ -96,6 +96,7 @@ namespace SaltsEnemies_Reseasoned
                 "Forest Bump",
                 "A Secret Animation Where If It Spawns Between The Two Of Them Like That It Passes The Ball Between Them",
                 "1 In 10000 Chance For It To Be Named Dennis",
+                "It Should Spawn A Mung If It Misses"
                 ];
 
             insider.CombatEnterEffects = [Effects.GenerateEffect(inside),
