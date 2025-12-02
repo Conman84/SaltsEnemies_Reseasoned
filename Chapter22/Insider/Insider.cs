65c590be681a9447fe0c39d62bed9466c4ddc6ae
Aprils <154932000+yttpr@users.noreply.github.com>
2025-12-01 16:38:24 -0800

    the sniper

--- Chapter22/Insider/Insider.cs
+++ Chapter22/Insider/Insider.cs
@@ -59,6 +59,7 @@ namespace SaltsEnemies_Reseasoned
                 "TAFHF-D5CLI-4TGV8",
                 "Jtkhfulakefrutysejw3oewuiyrdfhkgmcsdhkafekaeaaeljreluy",
                 "Papereater",
+                "The Sniper"
                 ];
             CasterRandomizeNameEnemyEffect outside = ScriptableObject.CreateInstance<CasterRandomizeNameEnemyEffect>();
             outside.PossibleNames = [
