22b88533c16a461b3b315472ee884abe5d494f3b
Aprils <154932000+yttpr@users.noreply.github.com>
2025-12-11 13:53:23 -0800

    panopticon encounter work

--- Chapter24/Panopticon/PanopticonEncounters.cs
+++ Chapter24/Panopticon/PanopticonEncounters.cs
@@ -17,6 +17,19 @@ namespace SaltsEnemies_Reseasoned
             med.RoarEvent = "event:/Hawthorne/Roar/TankRoar";
 
             med.SimpleAddEncounter(4, "Panopticon_EN");
+            med.SimpleAddEncounter(2, "Panopticon_EN", 2, "InHisImage_EN");
+            med.SimpleAddEncounter(2, "Panopticon_EN", 2, "PawnA_EN");
+            med.SimpleAddEncounter(2, "Panopticon_EN", 1, "InHisImage_EN", 1, "InHerImage_EN");
+            med.SimpleAddEncounter(3, "Panopticon_EN", 1, Enemies.Shivering);
+            med.SimpleAddEncounter(2, "Panopticon_EN", 1, Enemies.Minister);
+            med.SimpleAddEncounter(2, "Panopticon_EN", 1, Flower.Blue, 1, Flower.Red);
+            med.SimpleAddEncounter(2, "Panopticon_EN", 3, "TortureMeNot_EN");
+            med.SimpleAddEncounter(2, "Panopticon_EN", 2, "Insider_EN");
+            med.SimpleAddEncounter(2, "Panopticon_EN", 2, "Sundowner_EN");
+            med.SimpleAddEncounter(2, "Panopticon_EN", 2, "EvilDog_EN");
+            med.SimpleAddEncounter(2, "Panopticon_EN", 2, "InHerImage_EN");
+            med.SimpleAddEncounter(3, "Panopticon_EN", 1, "BlackStar_EN");
+            med.SimpleAddEncounter(3, "Panopticon_EN", 1, "Grandfather_EN");
 
             med.AddEncounterToDataBases();
             EnemyEncounterUtils.AddEncounterToZoneSelector(Garden.H.Panopticon.Med, 10, ZoneType_GameIDs.Garden_Hard, BundleDifficulty.Medium);
