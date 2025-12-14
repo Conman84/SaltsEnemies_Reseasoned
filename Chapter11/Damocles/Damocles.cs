5d1396428f3387c3846c3d0f130c588abe1956ad
Aprils <154932000+yttpr@users.noreply.github.com>
2025-12-14 01:38:20 -0800

    the right damocles goes into the bronzo pool now.

--- Chapter11/Damocles/Damocles.cs
+++ Chapter11/Damocles/Damocles.cs
@@ -134,6 +134,7 @@ namespace SaltsEnemies_Reseasoned
             EnemySO clone = UnityEngine.Object.Instantiate(LoadedAssetsHandler.GetEnemy("Damocles_EN"));
             clone.passiveAbilities = [Passives.Formless, damocles, Passives.Absorb, mf, Passives.Withering];
             EnemyUtils.AddEnemyToHealthSpawnPool(clone, PoolList_GameIDs.Sepulchre);
+            EnemyUtils.AddEnemyToHealthSpawnPool(clone, PoolList_GameIDs.Bronzo);
         }
     }
 }
