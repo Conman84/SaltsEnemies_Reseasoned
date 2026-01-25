649c560fc24703ee418cb66ca6cb93a4391bf6a7
AprilsFoolsDay <154932000+yttpr@users.noreply.github.com>
2026-01-24 16:01:11 -0800

    fail

--- Chapter24/Ecstasy/Ecstasy99Encounters.cs
+++ Chapter24/Ecstasy/Ecstasy99Encounters.cs
@@ -10,7 +10,7 @@ namespace SaltsEnemies_Reseasoned
     {
         public static void Add()
         {
-            Portals.AddPortalSign("Salt_TemplateEncounter_Sign", ResourceLoader.LoadSprite("GrayEcstasyWorld.png"), Portals.EnemyIDColor);
+            Portals.AddPortalSign("Salt_Ecstasy99Encounter_Sign", ResourceLoader.LoadSprite("GrayEcstasyWorld.png"), Portals.EnemyIDColor);
 
             EnemyEncounter_API med = new EnemyEncounter_API(EncounterType.Random, Garden.H.Ecstasy99.Med, "Salt_Ecstasy99Encounter_Sign");
             med.MusicEvent = "event:/Hawthorne/EcstasySong";
