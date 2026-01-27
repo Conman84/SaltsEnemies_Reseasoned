907b1bed383d9b230f70030995f0324288d226cf
AprilsFoolsDay <154932000+yttpr@users.noreply.github.com>
2026-01-26 22:13:00 -0800

    name

--- Scripts/GeneralConfig.cs
+++ Scripts/GeneralConfig.cs
@@ -9,7 +9,7 @@ namespace SaltsEnemies_Reseasoned
         public static ConfigEntry<bool> MidnightStoplightTrainAnimation;
         public static void Setup()
         {
-            ConfigFile seatbelt = new ConfigFile(Path.Combine(Paths.ConfigPath, "SaltEnemiesTMConfigurationFile.cfg"), true);
+            ConfigFile seatbelt = new ConfigFile(Path.Combine(Paths.ConfigPath, "SaltEnemiesTmConfigurationFile.cfg"), true);
             MidnightStoplightTrainAnimation = seatbelt.Bind<bool>("Specific", "MidnightStoplight_TrainAnimation", true, "While true, the enemy Mindight Traffic Light will use a custom Train animation. While false, it will use Clobber's animation instead.");
         }
     }
