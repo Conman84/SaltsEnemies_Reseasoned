70ff0736aef51bf431b0ff9a7f8b2200dcf57793
AprilsFoolsDay <154932000+yttpr@users.noreply.github.com>
2026-03-26 22:10:45 -0700

    guh

--- Scripts/ResourceLoaderHook.cs
+++ Scripts/ResourceLoaderHook.cs
@@ -25,9 +25,9 @@ namespace SaltsEnemies_Reseasoned
 
 public static class ResourceLoader
 {
-    public static Sprite LoadSprite(string name, Vector2? pivot = null, int ppu = 32)
+    public static Sprite LoadSprite(string name, Vector2? pivot = null, int ppu = 32, Assembly assembly = null)
     {
-        Sprite ret = BrutalAPI.ResourceLoader.LoadSprite(name, pivot, ppu);
+        Sprite ret = BrutalAPI.ResourceLoader.LoadSprite(name, pivot, ppu, assembly);
 
         if (ret == null) Debug.LogError("FAILED TO GET SPRITE FOR: " + name);
 
