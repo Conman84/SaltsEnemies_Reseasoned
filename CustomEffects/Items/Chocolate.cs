d36500fc688fb716497061bf80154fa43eea0eb7
AprilsFoolsDay <154932000+yttpr@users.noreply.github.com>
2026-03-26 22:09:19 -0700

    items..

--- CustomEffects/Items/Chocolate.cs
+++ CustomEffects/Items/Chocolate.cs
@@ -73,4 +73,32 @@ namespace SaltsEnemies_Reseasoned
             return !CombatManager.Instance._stats.combatSlots.EnemySlots[effector.SlotID].HasUnit;
         }
     }
+
+    public class AddPassiveWithDisplayEffect : EffectSO
+    {
+        public BasePassiveAbilitySO passive;
+        public override bool PerformEffect(CombatStats stats, IUnit caster, TargetSlotInfo[] targets, bool areTargetSlots, int entryVariable, out int exitAmount)
+        {
+            exitAmount = 0;
+            List<int> ids = [];
+            List<bool> ischars = [];
+            List<string> passives = [];
+            List<Sprite> sprites = [];
+            foreach (TargetSlotInfo target in targets)
+            {
+                if (target.HasUnit && target.Unit.AddPassiveAbility(passive))
+                {
+                    exitAmount++;
+                    ids.Add(target.Unit.ID);
+                    ischars.Add(target.Unit.IsUnitCharacter);
+                    passives.Add(passive.GetPassiveLocData().text + " Added");
+                    sprites.Add(passive.passiveIcon);
+                }
+            }
+
+            CombatManager.Instance.AddUIAction(new ShowMultiplePassiveInformationUIAction(ids.ToArray(), ischars.ToArray(), passives.ToArray(), sprites.ToArray()));
+
+            return exitAmount > 0;
+        }
+    }
 }
