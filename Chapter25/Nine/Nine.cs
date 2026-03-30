425a7c09e2f1fda52cd571a9596fef517f8434a4
AprilsFoolsDay <154932000+yttpr@users.noreply.github.com>
2026-03-29 17:12:41 -0700

    finished nine

---
+++ Chapter25/Nine/Nine.cs
@@ -0,0 +1,128 @@
+﻿using BrutalAPI;
+using SaltEnemies_Reseasoned;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace SaltsEnemies_Reseasoned
+{
+    public static class Nine
+    {
+        public static void Add()
+        {
+            Enemy nine = new Enemy("NINE", "Nine_EN")
+            {
+                Health = 9,
+                HealthColor = Pigments.Red,
+                CombatSprite = ResourceLoader.LoadSprite("NineIcon.png"),
+                OverworldAliveSprite = ResourceLoader.LoadSprite("NineWorld.png", new Vector2(0.5f, 0f), 32),
+                OverworldDeadSprite = ResourceLoader.LoadSprite("NineDead.png", new Vector2(0.5f, 0f), 32),
+                DamageSound = LoadedAssetsHandler.GetEnemy("Hauntling_EN").damageSound,
+                DeathSound = LoadedAssetsHandler.GetEnemy("Hauntling_EN").deathSound,
+            };
+            nine.PrepareEnemyPrefab("Assets/Abyss/Nine_Enemy.prefab", SaltsReseasoned.Meow, SaltsReseasoned.Meow.LoadAsset<GameObject>("Assets/Abyss/Nine_Gibs.prefab").GetComponent<ParticleSystem>());
+
+            //control
+            RandomTriggerPassive control = ScriptableObject.CreateInstance<RandomTriggerPassive>();
+            control.name = "Control_PA";
+            control._passiveName = "Control";
+            control.m_PassiveID = "Control_PA";
+            control.passiveIcon = ResourceLoader.LoadSprite("ControlPassive.png");
+            control._enemyDescription = "This enemy decides when it gets to move.";
+            control._characterDescription = "This party member randomly moves Left or Right.";
+            control.UniqueCall = (TriggerCalls)2658153;
+            control._triggerOn = [control.UniqueCall];
+            control.doesPassiveTriggerInformationPanel = true;
+            control.conditions = [];
+            control.effects = [Effects.GenerateEffect(ScriptableObject.CreateInstance<CasterSwapToSidesUpToEntryVariableEffect>(), 4, Slots.Self)];
+            control._min = 0.5f;
+            control._max = 5f;
+
+            //SURVIVAL
+            AnimationVisualsEffect core = ScriptableObject.CreateInstance<AnimationVisualsEffect>();
+            core._visuals = ((AnimationVisualsEffect)((PerformEffectWearable)LoadedAssetsHandler.GetWearable("DemonCore_SW")).effects[0].effect)._visuals;
+            core._animationTarget = Targeting.Slot_SelfSlot;
+            PerformEffectImmediatePassiveAbility survival = ScriptableObject.CreateInstance<PerformEffectImmediatePassiveAbility>();
+            survival._passiveName = "Survival Instinct (9)";
+            survival.passiveIcon = ResourceLoader.LoadSprite("survival.png");
+            survival._enemyDescription = "On death, deal an Agonizing amount of damage to all party members. \nDoes not trigger on Withering.";
+            survival._characterDescription = "On death, deal 9 damage to all enemies. \nDoes not trigger on Withering";
+            survival.m_PassiveID = "Survival_Instinct_PA";
+            survival.doesPassiveTriggerInformationPanel = true;
+            survival._triggerOn = new TriggerCalls[] { TriggerCalls.OnDeath };
+            Targetting_ByUnit_Side allEnemy = ScriptableObject.CreateInstance<Targetting_ByUnit_Side>();
+            allEnemy.getAllies = false;
+            allEnemy.getAllUnitSlots = false;
+            DeathReferenceDetectionEffectorCondition noWither = ScriptableObject.CreateInstance<DeathReferenceDetectionEffectorCondition>();
+            noWither._useWithering = true;
+            noWither._witheringDeath = false;
+            survival.conditions = new EffectorConditionSO[]
+            {
+                noWither
+            };
+            survival.effects = new EffectInfo[]
+            {
+                Effects.GenerateEffect(core, 1, Targeting.Slot_SelfSlot),
+                Effects.GenerateEffect(ScriptableObject.CreateInstance<DamageEffect>(), 9, Targeting.Unit_AllOpponents)
+            };
+
+            //FLITHERING
+            PerformEffectPassiveAbility flither = ScriptableObject.CreateInstance<PerformEffectPassiveAbility>();
+            flither._passiveName = "Flithering";
+            flither.passiveIcon = ResourceLoader.LoadSprite("FlitheringIcon.png");
+            flither.m_PassiveID = FlitheringHandler.Flithering;
+            flither._enemyDescription = "On any enemy dying, if there are no other enemies without \"Withering\" or \"Flithering\" as passives, instantly flee.\n" +
+                "At the start and end of the enemies' turn, if there are no other enemies without \"Cowardice\" or \"Flithering\" as passives, instantly flee.";
+            flither._characterDescription = "doesnt work";
+            flither.doesPassiveTriggerInformationPanel = false;
+            flither.effects = new EffectInfo[] { Effects.GenerateEffect(RootActionEffect.Create(new EffectInfo[]
+            {
+                Effects.GenerateEffect(ScriptableObject.CreateInstance<CowardEffect>(), 1, Slots.Self)
+            }), 1, Slots.Self) };
+            flither._triggerOn = new TriggerCalls[] { TriggerCalls.OnPlayerTurnEnd_ForEnemy, TriggerCalls.OnRoundFinished };
+            flither.conditions = new EffectorConditionSO[]
+            {
+                ScriptableObject.CreateInstance<CowardCondition>()
+            };
+
+            nine.AddPassives(new BasePassiveAbilitySO[] { control, survival, flither });
+
+            Targetting_ByUnit_Side_Empty fools = ScriptableObject.CreateInstance<Targetting_ByUnit_Side_Empty>();
+            fools.getAllUnitSlots = true;
+            fools.getAllies = false;
+
+            Targetting_ByUnit_Side_Empty enemies = ScriptableObject.CreateInstance<Targetting_ByUnit_Side_Empty>();
+            fools.getAllUnitSlots = true;
+            fools.getAllies = true;
+
+            Ability mines = new Ability("Minesweeper", "Nine_Minsweeper_A");
+            mines.Description = "Apply 1 Slip to every unoccupied party member and enemy position.";
+            mines.Rarity = Rarity.Common;
+            mines.Effects = [
+                Effects.GenerateEffect(ScriptableObject.CreateInstance<ApplySlipSlotEffect>(), 1, enemies),
+                Effects.GenerateEffect(ScriptableObject.CreateInstance<ApplySlipSlotEffect>(), 1, fools),
+                ];
+            mines.AddIntentsToTarget(enemies, [Slip.Intent]);
+            mines.AddIntentsToTarget(fools, [Slip.Intent]);
+            mines.AnimationTarget = Slots.Self;
+            mines.Visuals = null;
+
+            Ability plus = new Ability("Plus", "Nine_Plus_A");
+            plus.Description = "Take Almost No damage.";
+            plus.Rarity = Rarity.Uncommon;
+            plus.Effects = [Effects.GenerateEffect(ScriptableObject.CreateInstance<DamageEffect>(), 1, Slots.Self)];
+            plus.AddIntentsToTarget(Slots.Self, ["Damage_1_2"]);
+            plus.AnimationTarget = Slots.Self;
+            plus.Visuals = null;
+
+            //ADD ENEMY
+            nine.AddEnemyAbilities(new EnemyAbilityInfo[]
+            {
+                mines.GenerateEnemyAbility(true),
+                plus.GenerateEnemyAbility(true),
+            });
+            nine.AddEnemy();
+        }
+    }
+}
---
+++ CustomEffects/Chapter25/NineEffects.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using SaltEnemies_Reseasoned;
+using HarmonyLib;
+
+namespace SaltsEnemies_Reseasoned
+{
+    public class RandomTriggerPassive : PerformEffectPassiveAbility
+    {
+        public TriggerCalls UniqueCall;
+        public float _min;
+        public float _max;
+
+        public Dictionary<IUnit, Coroutine> Coroutines;
+
+        public override void OnPassiveConnected(IUnit unit)
+        {
+            base.OnPassiveConnected(unit);
+            CombatManager.Instance.AddRootAction(new StartTimerAction(unit, this));
+        }
+        public override void OnPassiveDisconnected(IUnit unit)
+        {
+            base.OnPassiveDisconnected(unit);
+            if (Coroutines.ContainsKey(unit))
+            {
+                CombatManager.Instance.StopCoroutine(Coroutines[unit]);
+                Coroutines.Remove(unit);
+            }
+        }
+
+        public static IEnumerator RandomTimer(IUnit unit, float min, float max, TriggerCalls call)
+        {
+            while (true)
+            {
+                yield return new WaitForSecondsRealtime(Random.Range(min, max));
+                CombatManager.Instance.PostNotification(call.ToString(), unit, null);
+            }
+        }
+
+        public class StartTimerAction : CombatAction
+        {
+            public IUnit Unit;
+            public RandomTriggerPassive Passive;
+
+            public StartTimerAction(IUnit unit, RandomTriggerPassive passive)
+            {
+                Unit = unit;
+                Passive = passive;
+            }
+
+            public override IEnumerator Execute(CombatStats stats)
+            {
+                Coroutine added = CombatManager.Instance.StartCoroutine(RandomTimer(Unit, Passive._min, Passive._max, Passive.UniqueCall));
+                if (Passive.Coroutines.ContainsKey(Unit))
+                {
+                    CombatManager.Instance.StopCoroutine(Passive.Coroutines[Unit]);
+                }
+                Passive.Coroutines[Unit] = added;
+                yield break;
+            }
+        }
+    }
+
+    public class CasterSwapToSidesUpToEntryVariableEffect : SwapToOneSideEffect
+    {
+        public override bool PerformEffect(CombatStats stats, IUnit caster, TargetSlotInfo[] targets, bool areTargetSlots, int entryVariable, out int exitAmount)
+        {
+            exitAmount = 0;
+
+            _swapRight = UnityEngine.Random.Range(0, 100) < 50;
+
+            int times = UnityEngine.Random.Range(1, entryVariable + 1);
+
+            for (int i = 0; i < times; i++)
+            {
+                if (base.PerformEffect(stats, caster, Slots.Self.GetTargets(stats.combatSlots, caster.SlotID, caster.IsUnitCharacter), Slots.Self.AreTargetSlots, entryVariable, out exitAmount))
+                    exitAmount++;
+            }
+
+            return exitAmount > 0;
+        }
+    }
+}
