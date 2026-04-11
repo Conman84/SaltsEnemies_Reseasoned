49c93ed94deae44edf90674e04876fe4a75744af
AprilsFoolsDay <154932000+yttpr@users.noreply.github.com>
2026-04-10 17:28:57 -0700

    #

--- SaltsReseasoned.cs
+++ SaltsReseasoned.cs
@@ -9,7 +9,7 @@ using SaltsEnemies_Reseasoned.Chapter1.LostSheep;
 namespace SaltsEnemies_Reseasoned
 {
 
-    [BepInPlugin("000.saltenemies", "Salt Enemies (TM) Reseasoned", "1.1.7.4")]//next is 1.1.7.4
+    [BepInPlugin("000.saltenemies", "Salt Enemies (TM) Reseasoned", "1.1.7.4")]//next is 1.1.7.5
     [BepInDependency("BrutalOrchestra.BrutalAPI", BepInDependency.DependencyFlags.HardDependency)]
     public class SaltsReseasoned : BaseUnityPlugin
     {
