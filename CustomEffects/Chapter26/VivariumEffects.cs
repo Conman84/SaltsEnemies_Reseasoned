fe69949be593cf22748f6b51eb64ffc4780172ef
AprilsFoolsDay <154932000+yttpr@users.noreply.github.com>
2026-05-16 17:29:32 -0700

    ok i did need this effect i guess yeah

--- CustomEffects/Chapter26/VivariumEffects.cs
+++ CustomEffects/Chapter26/VivariumEffects.cs
@@ -8,6 +8,7 @@ using JetBrains.Annotations;
 using System.Collections;
 using UnityEngine;
 using SaltEnemies_Reseasoned;
+using BrutalAPI;
 
 namespace SaltsEnemies_Reseasoned
 {
@@ -35,5 +36,38 @@ namespace SaltsEnemies_Reseasoned
 
         public float Rage;
 
+    }
+    public class RandomizeTargetHealthColorsNotCasterEffect : EffectSO
+    {
+        public List<ManaColorSO> options;
+        public override bool PerformEffect(CombatStats stats, IUnit caster, TargetSlotInfo[] targets, bool areTargetSlots, int entryVariable, out int exitAmount)
+        {
+            exitAmount = 0;
+            if (options == null)
+            {
+                options = new List<ManaColorSO>() { Pigments.Red, Pigments.Blue, Pigments.Yellow, Pigments.Purple };
+            }
+            foreach (TargetSlotInfo target in targets)
+            {
+                if (target.HasUnit && target.Unit.SlotID != caster.SlotID && target.Unit.HealthColor == caster.HealthColor)
+                {
+                    List<ManaColorSO> list = [.. options];
+                    if (list.Contains(target.Unit.HealthColor)) list.Remove(target.Unit.HealthColor);
+                    if (list.Contains(caster.HealthColor)) list.Remove(caster.HealthColor);
+
+                    if (list.Count >= 0) continue;
+
+                    if (target.Unit.ChangeHealthColor(list.GetRandom())) exitAmount++;
+                }
+            }
+            return exitAmount > 0;
+        }
+        public static RandomizeTargetHealthColorsNotCasterEffect Create(bool grey = false)
+        {
+            RandomizeTargetHealthColorsNotCasterEffect ret = ScriptableObject.CreateInstance<RandomizeTargetHealthColorsNotCasterEffect>();
+            ret.options = new List<ManaColorSO>() { Pigments.Red, Pigments.Blue, Pigments.Yellow, Pigments.Purple };
+            if (grey) ret.options.Add(Pigments.Grey);
+            return ret;
+        }
     }
 }
