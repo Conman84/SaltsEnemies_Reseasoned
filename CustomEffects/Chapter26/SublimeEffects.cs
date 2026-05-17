e872ecaaf29a746ba5729767a92d11a526c1b431
AprilsFoolsDay <154932000+yttpr@users.noreply.github.com>
2026-05-16 17:27:49 -0700

    sublime and vivarium effects

---
+++ CustomEffects/Chapter26/SublimeEffects.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace SaltsEnemies_Reseasoned
+{
+    public class ZeroOrEntryEffect : EffectSO
+    {
+        public EffectSO effect;
+        public override bool PerformEffect(CombatStats stats, IUnit caster, TargetSlotInfo[] targets, bool areTargetSlots, int entryVariable, out int exitAmount)
+        {
+            exitAmount = 0;
+            foreach (TargetSlotInfo target in targets)
+            {
+                bool yes = UnityEngine.Random.Range(0, 100) < 50;
+
+                effect.PerformEffect(stats, caster, [target], areTargetSlots, !yes ? 0 : entryVariable, out int exit);
+                exitAmount += exit;
+            }
+
+            return exitAmount > 0;
+        }
+
+        public static ZeroOrEntryEffect Create(EffectSO effect)
+        {
+            ZeroOrEntryEffect ret = ScriptableObject.CreateInstance<ZeroOrEntryEffect>();
+            ret.effect = effect;
+            return ret;
+        }
+
+        public class TargettingFurthestSide : BaseCombatTargettingSO
+        {
+            public bool getAllies;
+            public override bool AreTargetAllies => getAllies;
+            public override bool AreTargetSlots => true;
+
+            public override TargetSlotInfo[] GetTargets(SlotsCombat slots, int casterSlotID, bool isCasterCharacter)
+            {
+                int size = 1;
+                if (!isCasterCharacter)
+                {
+                    foreach (CombatSlot slot in slots.EnemySlots)
+                    {
+                        if (slot.SlotID == casterSlotID)
+                        {
+                            if (slot.HasUnit) size = slot.Unit.Size;
+                            break;
+                        }
+                    }
+                }
+
+                List<TargetSlotInfo> left = [];
+                List<TargetSlotInfo> right = [];
+
+                foreach (CombatSlot slot in isCasterCharacter == getAllies ? slots.CharacterSlots : slots.EnemySlots)
+                {
+                    if (slot.HasUnit)
+                    {
+                        if (slot.SlotID < casterSlotID) left.Add(slot.TargetSlotInformation);
+                        else if (slot.SlotID > casterSlotID + size - 1) right.Add(slot.TargetSlotInformation);
+                    }
+                }
+
+                if (left.Count > right.Count) return left.ToArray();
+                else if (right.Count > left.Count) return right.ToArray();
+                else
+                {
+                    left.AddRange(right);
+                    return left.ToArray();
+                }
+            }
+        }
+    }
+}
---
+++ CustomEffects/Chapter26/VivariumEffects.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MonoMod.RuntimeDetour;
+using System.Reflection;
+using UnityEngine.EventSystems;
+using JetBrains.Annotations;
+using System.Collections;
+using UnityEngine;
+using SaltEnemies_Reseasoned;
+
+namespace SaltsEnemies_Reseasoned
+{
+    public class MouseHover_EnemyInFieldLayout : EnemyInFieldLayout
+    {
+        public static TriggerCalls Trigger => (TriggerCalls)1095224;
+        public void Update()
+        {
+            if (MouseSelected) Rage -= Time.deltaTime * 5;
+            else Rage += Time.deltaTime;
+
+            if (Rage < 0f) Rage = 0f;
+
+            if (!CombatManager.Instance._stats.IsPlayerTurn) Rage = 0f;
+
+            if (Rage > 5f)
+            {
+                Rage = 4f;
+                foreach (EnemyCombat enemy in CombatManager.Instance._stats.EnemiesOnField.Values)
+                {
+                    if (enemy.ID == EnemyID) CombatManager.Instance.PostNotification(Trigger.ToString(), enemy, null);
+                }
+            }
+        }
+
+        public float Rage;
+
+    }
+}
